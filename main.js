9358ab101b7c02f6ba11a3af31ea8452eef7f66f
Pete Widders <pwidders@hotmail.com>
2018-12-18 22:41:38 -0800

    return functionality complete

--- main.js
+++ main.js
@@ -42,13 +42,11 @@ function mainMenu () {
             break;
           // call on returnIt
           case "Return a movie":
-            queryItStock();
-            returnIt();
+            queryItReturn();
             break;
           // call on stockIt
           case "Admin: inventory manager":
-              queryIt();
-              stockIt();
+              authItAdmin();
               break;
         }
     }) 
@@ -67,6 +65,28 @@ function authIt() {
     .then(function () {
          console.log("\n Thanks. Here's our current films:");
         queryIt();
+    })
+}
+
+// "Phone number login"
+function authItAdmin() {
+    inquirer.prompt([
+        {
+        type: "input",
+        message: "Enter admin password: ",
+        name: "adminPass",    
+        }
+    ])
+    .then(function (userChoice) {
+        var adminPass = "password";
+        var enteredPass = userChoice.adminPass;
+        if (enteredPass === adminPass) {
+            console.log("\n You shall pass");
+            queryItAdmin();
+        }
+        else {
+            console.log("Password Incorrect")
+        }
     })
 }
 
@@ -126,5 +146,81 @@ function rentIt() {
             }
         })
     })
+}
+
+function queryItReturn() {
+    // Query the database to present full UI menu
+    connection.query("SELECT * FROM movies", function(err, res, fields) {
+      if (err) throw err;
+      console.log("\nID #" + " | " + "Movie Name" + " | " + "In-stock" + " | \n");
+      for (var i = 0; i < res.length; i++) {
+        console.log(res[i].id + "  |  " + res[i].movie_name + "  |  " + res[i].in_stock + "  | \n");
+      }
+      console.log("------------------------------");
+      returnIt()
+    }) 
+}
+
+function returnIt() {
+    // Prompt user for input
+    inquirer.prompt([
+        {
+        type: "input",
+        message: "Please enter movie selection's ID: ",
+        name: "movieID",    
+        }
+    ])
+    .then(function (userChoice) {
+        var movie = userChoice.movieID;
+        console.log("You've chosen " + movie);
+        connection.query("SELECT in_stock FROM movies WHERE id = " + parseInt(movie), function (err, res) {
+            if (err) throw err;
+            // update statment
+            var movieStock = res;
+            var newMovieStock = movieStock[0].in_stock;
+            // update statment
+            var sql = `UPDATE movies
+            SET in_stock = ?
+            WHERE id = ?`;
+            var data = [newMovieStock + 1, movie];
+        
+            // execute the UPDATE statement
+            connection.query(sql, data, (error, results,) => {
+                if (error){
+                    return console.error(error.message);
+                }
+                console.log("Movie returned! Always be kind and rewind." + 'Rows affected:', results.affectedRows);
+            });
+            console.log("-------------------------------------------");
+        })
+        //mainMenu();
+    })
+}
+
+function queryItAdmin() {
+    // Query the database to present full UI menu
+    connection.query("SELECT * FROM movies", function(err, res, fields) {
+      if (err) throw err;
+      console.log("\nID #" + " | " + "Movie Name" + " | " + "Genre" + " | " + "In-stock" + " | " + "Price" + " | " + "Rating" + " | \n");
+      for (var i = 0; i < res.length; i++) {
+        console.log(res[i].id + "  |  " + res[i].movie_name + "  |  " + res[i].genre + "  |  " + res[i].in_stock + "  |  " + res[i].price + "  |  " + res[i].rating + "  |  \n");
+      }
+      console.log("-------------------------------------------");
+      stockIt()
+    }) 
+}
+
+function stockIt() {
+// Inquirer user menu
+    inquirer
+    .prompt([
+        /* Pass your questions in here */
+        {
+            type: "list",
+            name: "menuChoice",
+            message: "Hi! Welcome to Mockbuster Video. Please select an option below:",
+            choices: ['Rent a movie', 'Return a movie', 'Admin: inventory manager']
+        }
+    ]).then
 }
 
